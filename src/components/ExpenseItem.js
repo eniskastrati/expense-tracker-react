67dd435a8e0928980aff5b0d17b80ee83f71bfa6
Enis Kastrati <enis@smartmile.eu>
2022-05-02 16:18:04 +0200

    ADD: ExpseneItem css file and UPDATED: ExpenseItem Js file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div className="">March 25th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$293.54</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
