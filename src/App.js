73e027e7beab6db86e81287a2c96c731e55697dd
eniskastrati <eniskastrati18@gmail.com>
2022-05-03 09:18:46 +0200

    UPDATE: removed comments

--- src/App.js
+++ src/App.js
@@ -29,26 +29,6 @@ function App() {
     <div>
       <h1>Expense Tracker React</h1>
       <NewExpensesComponent data={expenses} />
-      {/* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
     </div>
   );
 }
