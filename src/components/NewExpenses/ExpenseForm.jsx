c74e90d10f7163c9afd765e7cd0568ee42d1b117
eniskastrati <eniskastrati18@gmail.com>
2022-05-04 10:57:48 +0200

    ADD: added value to input tags

--- src/components/NewExpenses/ExpenseForm.jsx
+++ src/components/NewExpenses/ExpenseForm.jsx
@@ -21,27 +21,27 @@ function ExpenseForm() {
   const submitHandler = (payload) => {
     payload.preventDefault();
 
-    const data = {
+    const expenseData = {
       title: enteredTitile,
       amount: enteredAmount,
       date: new Date(enteredDate) 
     };
-    console.log(data)
+    console.log(expenseData)
   }
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" onChange={changeHandlerOne} />
+          <input type="text" value={enteredTitile} onChange={changeHandlerOne} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={changeHandlerTwo} />
+          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={changeHandlerTwo} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-13" onChange={changeHandlerThree}/>
+          <input type="date" min="2019-01-01" max="2022-12-13" value={enteredDate} onChange={changeHandlerThree}/>
         </div>
       </div>
       <div className="new-expense__acitons">
