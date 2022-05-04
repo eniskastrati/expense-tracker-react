b744cf8053ace3f40de1f16242d0259d1a57be89
eniskastrati <eniskastrati18@gmail.com>
2022-05-04 10:54:54 +0200

    ADD: addes a specific handler for each input

--- src/components/NewExpenses/ExpenseForm.jsx
+++ src/components/NewExpenses/ExpenseForm.jsx
@@ -6,26 +6,42 @@ function ExpenseForm() {
   const [enteredTitile, setEnterdTitle] = useState('');
   const [enteredAmount, setEnterdAmount] = useState('');
   const [enteredDate, setEnterdDate] = useState('');
-  const changeHandler = (e) => {
+  const changeHandlerOne = (e) => {
     e.preventDefault();
     setEnterdTitle(e.target.value);
+  };
+  const changeHandlerTwo = (e) => {
+    e.preventDefault();
     setEnterdAmount(e.target.value);
+  };
+  const changeHandlerThree = (e) => {
+    e.preventDefault();
     setEnterdDate(e.target.value);
   };
+  const submitHandler = (payload) => {
+    payload.preventDefault();
+
+    const data = {
+      title: enteredTitile,
+      amount: enteredAmount,
+      date: new Date(enteredDate) 
+    };
+    console.log(data)
+  }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" onChange={changeHandler} />
+          <input type="text" onChange={changeHandlerOne} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={changeHandler} />
+          <input type="number" min="0.01" step="0.01" onChange={changeHandlerTwo} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-13" onChange={changeHandler}/>
+          <input type="date" min="2019-01-01" max="2022-12-13" onChange={changeHandlerThree}/>
         </div>
       </div>
       <div className="new-expense__acitons">
