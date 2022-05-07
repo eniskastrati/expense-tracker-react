cc7fa38e0ee7764daef213be76f4bf243de0e9ee
eniskastrati <eniskastrati18@gmail.com>
2022-05-08 00:05:06 +0200

    UPDATE: prettier changes

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,19 +1,22 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpensesFilter from "../NewExpenses/ExpenseFileter/ExpenseFilter";
+import ExpensesFilter from "../NewExpenses/ExpenseFilter/ExpenseFilter";
 
 function NewExpensesComponent({ ...props }) {
-  const [filterdYear, setFilteredYear] = useState('2020');
+  const [filterdYear, setFilteredYear] = useState("2020");
 
   const getSelectedYearValue = (yearValue) => {
-    setFilteredYear(yearValue)
+    setFilteredYear(yearValue);
   };
   return (
     <>
       <Card className="expenses">
-        <ExpensesFilter selected={filterdYear} onSelectedYearValue={getSelectedYearValue} />
+        <ExpensesFilter
+          selected={filterdYear}
+          onSelectedYearValue={getSelectedYearValue}
+        />
         {props.data.map((expense) => {
           return (
             <ExpenseItem
