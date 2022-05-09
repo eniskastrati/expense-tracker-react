7cc406a39828fc77b30ea68cb10add7e6b95cbf6
eniskastrati <eniskastrati18@gmail.com>
2022-05-09 15:30:47 +0200

    UPDATE: impot new component

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../NewExpenses/ExpenseFilter/ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 function NewExpensesComponent({ ...props }) {
   const [filterdYear, setFilteredYear] = useState("2020");
@@ -15,6 +16,21 @@ function NewExpensesComponent({ ...props }) {
     return expense.date.getFullYear().toString() === filterdYear;
   });
 
+  let expenseContent = <p style={{color: "white"}}>No Expenses foung. </p>;
+
+  if(filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    })
+  }
+
   return (
     <>
       <Card className="expenses">
@@ -22,19 +38,7 @@ function NewExpensesComponent({ ...props }) {
           selected={filterdYear}
           onSelectedYearValue={getSelectedYearValue}
         />
-        {filteredExpenses && <p style={{color: "white"}}>No Expenses foung. </p>}
-        {filteredExpenses.length > 0 && (
-          filteredExpenses.map((expense) => {
-            return (
-              <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-              />
-            );
-          })
-        )}
+        <ExpensesList data={filteredExpenses}/>
       </Card>
     </>
   );
