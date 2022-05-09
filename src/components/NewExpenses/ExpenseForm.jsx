2c314647eddfcb3281aacd9ccdb71b9e2d0005ab
eniskastrati <eniskastrati18@gmail.com>
2022-05-09 16:55:40 +0200

    UPDATE: adding closetoggle functinonality to New Expense

--- src/components/NewExpenses/ExpenseForm.jsx
+++ src/components/NewExpenses/ExpenseForm.jsx
@@ -6,6 +6,7 @@ function ExpenseForm(props) {
   const [enteredTitile, setEnterdTitle] = useState("");
   const [enteredAmount, setEnterdAmount] = useState("");
   const [enteredDate, setEnterdDate] = useState("");
+  const [showForm, setShowForm] = useState(false);
   const changeHandlerOne = (e) => {
     e.preventDefault();
     setEnterdTitle(e.target.value);
@@ -27,45 +28,59 @@ function ExpenseForm(props) {
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
+    resetHandle();
+  };
+  const resetHandle = () => {
     setEnterdTitle("");
     setEnterdAmount("");
     setEnterdDate("");
   };
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            value={enteredTitile}
-            onChange={changeHandlerOne}
-          />
+    <form onSubmit={submitHandler} onReset={resetHandle}>
+      {showForm ? (
+        <>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                value={enteredTitile}
+                onChange={changeHandlerOne}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label htmlFor="amount">Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredAmount}
+                onChange={changeHandlerTwo}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-13"
+                value={enteredDate}
+                onChange={changeHandlerThree}
+              />
+            </div>
+          </div>
+          <div className="new-expense__acitons">
+            <button type="reset" onClick={() => setShowForm(false)}>
+              Cancel
+            </button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </>
+      ) : (
+        <div className="new-expense__acitons">
+          <button onClick={() => setShowForm(true)}>Add New Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label htmlFor="amount">Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={changeHandlerTwo}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-13"
-            value={enteredDate}
-            onChange={changeHandlerThree}
-          />
-        </div>
-      </div>
-      <div className="new-expense__acitons">
-        <button type="submit">Add Expense</button>
-      </div>
+      )}
     </form>
   );
 }
