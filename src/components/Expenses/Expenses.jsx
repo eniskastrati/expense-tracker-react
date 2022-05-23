a5e8552cf9b35b8c578c3772f179a7d80554679f
eniskastrati <eniskastrati18@gmail.com>
2022-05-23 21:59:42 +0200

    saving stage state

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../NewExpenses/ExpenseFilter/ExpenseFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function NewExpensesComponent({ ...props }) {
   const [filterdYear, setFilteredYear] = useState("2020");
@@ -23,6 +24,7 @@ function NewExpensesComponent({ ...props }) {
           selected={filterdYear}
           onSelectedYearValue={getSelectedYearValue}
         />
+        <ExpensesChart expense={filteredExpenses} />
         <ExpensesList data={filteredExpenses} />
       </Card>
     </li>
