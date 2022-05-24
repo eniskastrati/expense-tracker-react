6cc2942c77c7e68434257f10f8379a2ee3cb8aa8
eniskastrati <eniskastrati18@gmail.com>
2022-05-24 09:12:12 +0200

    fixing writting mistakes

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -5,14 +5,14 @@ import "./Chart.css";
 
 function Chart(props) {
   const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-  const totatlMaximum = Math.max(...dataPointsValues);
+  const totalMaximum = Math.max(...dataPointsValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={totatlMaximum}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
