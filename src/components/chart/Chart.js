badf4ccda0a1a508945ccb2002b1e7e17b850828
eniskastrati <eniskastrati18@gmail.com>
2022-05-24 08:59:47 +0200

    props label parsing fixed

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import ChartBar from './ChartBar';
-import './Chart.css'
+import ChartBar from "./ChartBar";
+import "./Chart.css";
 
 function Chart(props) {
-    const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value);
-    const totoalMaximun = Math.max(...dataPointsValues);
-    return (
-        <div className='chart'>
-            {props.dataPoints.map(dataPoint => <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totoalMaximun} label={dataPoint.lable} />)}
-        </div>
-    )
+  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totatlMaximum = Math.max(...dataPointsValues);
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totatlMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Chart
+export default Chart;
